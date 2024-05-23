1cb24b76dd493a642fd8cb8de4d9e5e84d782862
Mete Ahmet Yakar <56269926+meteahmetyakar@users.noreply.github.com>
2024-05-24 00:37:26 +0300

    project uploaded

--- demo/src/main/java/com/ekip6/BestPriceGuarantee/dto/FlightDTO.java
+++
@@ -1,6 +0,0 @@
-package com.ekip6.BestPriceGuarantee.dto;
-
-import java.time.LocalDateTime;
-
-public record FlightDTO(Long flightId, String origin, String destination, LocalDateTime departureTime) {
-}
--- demo/src/main/java/com/ekip6/BestPriceGuarantee/dto/PriceHistoryDTO.java
+++
@@ -1,7 +0,0 @@
-package com.ekip6.BestPriceGuarantee.dto;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-public record PriceHistoryDTO(Long priceId, Long flightId, BigDecimal price, LocalDateTime updateDate) {
-}
